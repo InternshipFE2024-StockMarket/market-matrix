74651885059e754045f1bc5a79f5b4a442b0587a
adrianacostep33 <adriana.coste@principal33.com>
2024-04-11 16:28:50 +0300

    Add template project

---
+++ MarketMatrix/App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
