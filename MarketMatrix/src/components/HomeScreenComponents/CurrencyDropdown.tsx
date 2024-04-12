93da3e7c0b3a0a0010d2cd4fb47cb8d2653e6ccf
ioana.girba <>
2024-04-12 15:36:55 +0300

    SMA-23 -- Create total value structure

---
+++ MarketMatrix/src/components/HomeScreenComponents/CurrencyDropdown.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export const CurrencyDropdown = () => {
+  return (
+    <View>
+      <Text>USD</Text>
+    </View>
+  );
+};
--- MarketMatrix/src/screens/HomeScreen.tsx
+++ MarketMatrix/src/screens/HomeScreen.tsx
@@ -2,12 +2,29 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Colors} from '../constants/Colors';
 import GradientBackground from '../components/UI/GradientBackground';
+import {CurrencyDropdown} from '../components/HomeScreenComponents/CurrencyDropdown';
 
 const HomeScreen = () => {
   return (
     <GradientBackground>
-      <View>
-        <Text style={styles.text}>HomeScreen</Text>
+      <View style={styles.homeWrapper}>
+        <View>
+          <Text style={styles.title}>Market Matrix</Text>
+        </View>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.text}>Your total value:</Text>
+            <View style={styles.valueContainer}>
+              <Text style={styles.currency}>$</Text>
+              <Text style={styles.value}>180,20.66</Text>
+            </View>
+            {/* create a component to change dinamically the color and add arrow icon  */}
+            <Text style={styles.valueDifference}>34.10 (0.19%)</Text>
+          </View>
+          <CurrencyDropdown />
+        </View>
+        <Text style={styles.text}>Stories</Text>
+        <Text style={styles.text}>Chart</Text>
       </View>
     </GradientBackground>
   );
@@ -16,5 +33,37 @@ const HomeScreen = () => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  text: {color: Colors.text500},
+  title: {
+    color: Colors.text500,
+    fontSize: 20,
+    alignSelf: 'center',
+  },
+  homeWrapper: {
+    padding: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 40,
+  },
+  text: {
+    color: Colors.text500,
+    fontSize: 16,
+  },
+  valueContainer: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+  },
+  currency: {
+    color: Colors.text500,
+    fontSize: 32,
+  },
+  value: {
+    color: Colors.text500,
+    fontSize: 44,
+  },
+  valueDifference: {
+    color: Colors.pink,
+    fontSize: 20,
+  },
 });
