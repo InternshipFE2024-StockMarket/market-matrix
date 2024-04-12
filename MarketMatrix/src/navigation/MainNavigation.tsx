a48cff803387c8c1fa56b23f9aedd4daa7636e61
ioana.girba <>
2024-04-12 14:20:24 +0300

    SMA-12 -- Add icons to tab screens

--- MarketMatrix/src/navigation/MainNavigation.tsx
+++ MarketMatrix/src/navigation/MainNavigation.tsx
@@ -3,8 +3,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import PortfolioScreen from '../screens/PortfolioScreen';
 import DiscoverScreen from '../screens/DiscoverScreen';
-import {StyleSheet} from 'react-native';
 import {Colors} from '../constants/Colors';
+import {NavigationIcon} from '../components/UI/NavigationIcon';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,21 +16,55 @@ const MainNavigation = () => {
         tabBarStyle: {
           backgroundColor: Colors.navigationBackground,
           borderTopWidth: 0,
+          paddingTop: 10,
         },
         tabBarActiveTintColor: Colors.selectedIcon,
         tabBarInactiveTintColor: Colors.text500,
       }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Portfolio" component={PortfolioScreen} />
-      <Tab.Screen name="Discover" component={DiscoverScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused, color}) => (
+            <NavigationIcon
+              focused={focused}
+              color={color}
+              size={28}
+              source={require('../assets/icons/icon-home.png')}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Portfolio"
+        component={PortfolioScreen}
+        options={{
+          tabBarIcon: ({focused, color}) => (
+            <NavigationIcon
+              focused={focused}
+              color={color}
+              size={28}
+              source={require('../assets/icons/icon-chart.png')}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Discover"
+        component={DiscoverScreen}
+        options={{
+          tabBarIcon: ({focused, color}) => (
+            <NavigationIcon
+              focused={focused}
+              color={color}
+              size={28}
+              source={require('../assets/icons/icon-search.png')}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
 export default MainNavigation;
-
-const styles = StyleSheet.create({
-  bottomNavigation: {
-    backgroundColor: Colors.navigationBackground,
-  },
-});
