67eeafccd90cc8fe956343064feab4f3a8277829
adrianacostep33 <adriana.coste@principal33.com>
2024-04-15 18:04:56 +0300

    SMA-57 -- Handle search input value

--- MarketMatrix/src/components/DiscoverScreen/SearchInput.tsx
+++ MarketMatrix/src/components/DiscoverScreen/SearchInput.tsx
@@ -1,24 +1,45 @@
-import {Image, StyleSheet, TextInput, View, ViewStyle} from 'react-native';
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  View,
+  ViewStyle,
+} from 'react-native';
 import React from 'react';
 import {Colors} from '../../constants/Colors';
+import {useSearchContext} from '../../contexts/searchContext';
+import {useNavigation} from '@react-navigation/native';
 
 interface InputProps {
   inputStyle?: ViewStyle;
 }
 
 const SearchInput = ({inputStyle}: InputProps) => {
+  const {inputValue, setInputValue} = useSearchContext();
+  const navigation = useNavigation();
+
+  const handleSubmit = () => {
+    navigation.navigate('Overview' as never);
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
         style={[styles.input, inputStyle]}
         placeholder="Search"
         placeholderTextColor={Colors.text600}
+        value={inputValue}
+        onChangeText={setInputValue}
+        onSubmitEditing={handleSubmit}
       />
-      <Image
-        tintColor={Colors.text500}
-        style={styles.search}
-        source={require('../../assets/icons/icon-search.png')}
-      />
+      <Pressable onPress={handleSubmit} style={styles.search}>
+        <Image
+          tintColor={Colors.text500}
+          style={styles.image}
+          source={require('../../assets/icons/icon-search.png')}
+        />
+      </Pressable>
     </View>
   );
 };
@@ -38,11 +59,16 @@ const styles = StyleSheet.create({
     width: '90%',
     height: 60,
     borderRadius: 10,
+    color: Colors.text500,
   },
   search: {
     width: 24,
     height: 24,
     position: 'absolute',
     right: 40,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
