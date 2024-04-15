5b130856031879038a1cbe1d77f05bba6ed222b6
adrianacostep33 <adriana.coste@principal33.com>
2024-04-15 20:27:02 +0300

    SMA-57 -- Implement search by ticker and company name

--- MarketMatrix/src/screens/DiscoverScreens/OverviewScreen.tsx
+++ MarketMatrix/src/screens/DiscoverScreens/OverviewScreen.tsx
@@ -4,11 +4,21 @@ import GradientBackground from '../../components/UI/GradientBackground';
 import {useStock} from '../../contexts/stocksContext';
 import {Stock} from '../../constants/Interfaces';
 import AssetItem from '../../components/DiscoverScreen/AssetItem';
+import {useSearchContext} from '../../contexts/searchContext';
 
 const OverviewScreen = () => {
   const stockContext = useStock();
-  const stocks = stockContext?.stocks;
-  console.log({stocks});
+  let assets = stockContext?.stocks;
+
+  const {inputValue} = useSearchContext();
+  const searchTerm = inputValue.trim().toLowerCase();
+
+  if (inputValue) {
+    assets = assets?.filter(
+      asset =>
+        asset.ticker === searchTerm || asset.companyName.includes(searchTerm),
+    );
+  }
 
   const renderAsset = (itemData: ListRenderItemInfo<Stock>) => {
     return <AssetItem stock={itemData.item} />;
@@ -17,7 +27,7 @@ const OverviewScreen = () => {
   return (
     <GradientBackground>
       <FlatList
-        data={stocks}
+        data={assets}
         keyExtractor={item => item.id}
         renderItem={renderAsset}
       />
