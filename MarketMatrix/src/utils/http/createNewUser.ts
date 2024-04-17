07428353bbfccaae0a0e820803683967f75c65ed
adrianacostep33 <adriana.coste@principal33.com>
2024-04-17 16:01:00 +0300

    SMA-94 -- Add input type

--- MarketMatrix/src/utils/http/createNewUser.ts
+++ MarketMatrix/src/utils/http/createNewUser.ts
@@ -2,8 +2,23 @@ import axios from 'axios';
 import {User} from '../../constants/Interfaces';
 
 export const createNewUser = async (newUser: User) => {
+  const userData = {
+    id: newUser.email,
+    name: newUser.name,
+    email: newUser.email,
+    password: 's3cur3P@ssw0rd',
+    investment: [
+      {
+        id: '',
+        ticker: '',
+        amount: 0,
+        boughtPrice: 0,
+        shares: 0,
+      },
+    ],
+  };
   try {
-    const response = await axios.post('http://localhost:3000/user', newUser);
+    const response = await axios.post('http://localhost:3000/user', userData);
     return response.data;
   } catch (err) {
     console.log(err);
