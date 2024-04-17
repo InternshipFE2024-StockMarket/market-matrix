64b38f6003d31ea0300dacdcb1b1f34dc0d0ef9a
adrianacostep33 <adriana.coste@principal33.com>
2024-04-17 11:13:58 +0300

    SMA-88 -- Improve landscape mode styling for Portfolio and Discover screens

--- MarketMatrix/src/components/PortfolioScreen/Header.tsx
+++ MarketMatrix/src/components/PortfolioScreen/Header.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import Button from '../UI/Button';
 import {getTotalPortofolioValue} from '../../utils/functions/getTotalPortofolioValue';
 import {Colors} from '../../constants/Colors';
@@ -8,10 +14,12 @@ import {useAuth} from '../../contexts/authContext';
 const Header = () => {
   let userPortfolioValue = Number(getTotalPortofolioValue()?.total);
   let plPortfolioValue = Number(getTotalPortofolioValue()?.difference);
-
+  const {width, height} = useWindowDimensions();
+  const isLandscape = width > height;
   const {logout} = useAuth();
-  return (
-    <SafeAreaView>
+
+  let content = (
+    <>
       <View style={styles.header}>
         <View style={styles.portfolioData}>
           <Text style={[styles.text, styles.portfolioValue]}>
@@ -29,8 +37,33 @@ const Header = () => {
         </View>
       </View>
       <Text style={[styles.text, styles.title]}>My Portfolio</Text>
-    </SafeAreaView>
+    </>
   );
+  if (isLandscape) {
+    content = (
+      <>
+        <View style={styles.landscapeHeader}>
+          <Text style={[styles.text, styles.landscapeTitle]}>My Portfolio</Text>
+          <View>
+            <Text style={[styles.text, styles.portfolioValue]}>
+              ${userPortfolioValue.toFixed(2)}
+            </Text>
+            <Text
+              style={{
+                color: plPortfolioValue > 0 ? Colors.green : Colors.pink,
+              }}>
+              {plPortfolioValue.toFixed(2)}
+            </Text>
+          </View>
+          <View>
+            <Button onPress={logout}>Logout</Button>
+          </View>
+        </View>
+      </>
+    );
+  }
+
+  return <SafeAreaView>{content}</SafeAreaView>;
 };
 
 export default Header;
@@ -41,6 +74,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginHorizontal: 10,
     marginBottom: 20,
+  },
+  landscapeHeader: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 5,
   },
   portfolioData: {
     justifyContent: 'center',
@@ -55,6 +93,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginVertical: 20,
     marginLeft: 10,
+  },
+  landscapeTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
   text: {
     color: Colors.text500,
